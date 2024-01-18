cfa8f2604d7de09a069ad2ccd07e4adf6da7904b
Thalita Alves <thalitaalves783@outlook.com>
2024-01-18 19:27:58 -0300

    estrutura de teste

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+context('Funcionalidade Login',()=>{
+    it('Deve gazer login com sucesso',()=>{
+
+    })
+
+    it('Deve exibir uma msg de erro ao inserir usuario ou senha inválido',()=>{
+        
+    })
+
+})
